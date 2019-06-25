3dfc91bd6ee5009deec80ab084d1eccc54e5017c
shiliang <shiliang1992@outlook.com>
2019-06-26 02:46:56 +0800

    201962624645

--- main/main.c
+++ main/main.c
@@ -25,3 +25,25 @@ void test() {
     printf("Hello # | world2!\a", 5);
 }
 
+/*
+ *  本 main 方法用于 举例说明触发了隐式类型转换
+ * int main()
+{
+    int i = -2;
+    unsigned int j = 1;
+
+    if( (i + j) >= 0 )
+    {
+        printf("i+j>=0\n");
+    }
+    else
+    {
+        printf("i+j<0\n");
+    }
+
+ //触发了隐式类型转换后的数据类型 用错误的 变量值的转换说明符 带来稀奇古怪的后果
+    //printf("i+j=%u\n", i + j);
+    printf("i+j=%d\n", i + j);
+
+    return 0;
+}*/
