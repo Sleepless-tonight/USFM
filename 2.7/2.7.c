66d25388f4f08350c2af25cdb647ba00803a0ca0
shiliang <shiliang@taetea.com.cn>
2021-12-03 18:48:38 +0800

    test

---
+++ 2.7/2.7.c
@@ -0,0 +1,15 @@
+//
+// Created by shili on 2021/12/3.
+//
+
+#include "stdio.h"
+
+int main(void) {
+    char a = 0;
+    scanf("%c", &a);
+    printf("num:%c", a);
+    int b = 0;
+    scanf("%d", &b);
+    printf("num:%d", b);
+    return 0;
+}
