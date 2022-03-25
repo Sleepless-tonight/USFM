f9c6f6559d708d81393991c8f549b6fdd920d48f
shiliang <22642862+Sleepless-tonight@users.noreply.github.com>
2022-03-25 16:55:55 +0800

    code c

---
+++ 8/8.5.1/main.c
@@ -0,0 +1,21 @@
+//
+// Created by shiliang on 2020-03-05.
+//
+//
+
+#define __STDC_WANT_LIB_EXT1__ 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define BUF_LEN 256         // Input buffer size
+#define INIT_NSTR 2         // Initial number of string
+#define NSTR_INCR 2         // increment to number to string
+
+
+int main (void)
+{
+    // Code in main () ...
+}
