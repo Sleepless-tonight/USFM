154d9d722685320a6f693d27effc4153a3603433
shiliang <22642862+Sleepless-tonight@users.noreply.github.com>
2022-04-06 19:21:16 +0800

    code c

--- 8/8.5.2/main.c
+++ 8/8.5.2/main.c
@@ -6,5 +6,19 @@
 long *IncomePlus(long *pPay);
 
 int main(void) {
+    long your_pay = 30000L;
+    long *pold_pay = &your_pay;
+    long *pnew_pay = NULL;
+    pnew_pay = IncomePlus(pold_pay);
+    printf("Old pay=$%ld\n", *pold_pay);
+    printf(" New pay =$%ld\n", *pnew_pay);
+    return 0;
 
 }
+
+long *IncomePlus(long *pPay) {
+    long pay = 0;
+    pay = *pPay + 10000L;
+    return &pay;
+}
+
